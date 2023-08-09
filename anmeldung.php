52dbf86861ea200dfeb04414f2ebc8c6fee5bafb
vievo7 <80112047+vievo7@users.noreply.github.com>
2023-08-09 04:47:06 +0200

    ok

--- anmeldung.php
+++ anmeldung.php
@@ -10,44 +10,79 @@
         <input type="text" name="username" id="username"><br>
         <label for="password">Passwort:</label><br>
         <input type="password" name="password" id="password"><br>
+        <input type="checkbox" name="remember" id="remember">
+        <label for="remember">Dieses Gerät für 30 Tage vertrauen</label><br>
         <input type="submit" name="login" value="Anmelden">
     </form>
 
-    <p>
     <?php
-        // Verbindung zur Datenbank herstellen
-        $db = new mysqli("hostname", "username", "password", "database_name");
+    // Hide errors
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
+    error_reporting(0);
 
-        // Überprüfen, ob die Verbindung erfolgreich war
-        if ($db->connect_error) {
-            die("Fehler beim Verbinden zur Datenbank: " . $db->connect_error);
-        }
+    // Connect to the database
+    try {
+        $db = new mysqli('hostname', 'username', 'password', 'database_name');
+    } catch (Exception $e) {
+        echo 'Fehler beim Verbinden zur Datenbank: ' . $e->getMessage();
+        exit;
+    }
 
-        // Überprüfen, ob das Formular abgeschickt wurde
-        if (isset($_POST["login"])) {
-            // Benutzerdaten aus dem Formular holen
-            $username = $_POST["username"];
-            $password = $_POST["password"];
+    // Check if a session is already started
+    if (session_status() == PHP_SESSION_ACTIVE) {
+        // The user is already logged in, redirect to homepage
+        header('Location: /index.php');
+        exit;
+    }
 
-            // Überprüfen, ob der Benutzername und das Passwort korrekt sind
-            $stmt = $db->prepare("SELECT * FROM users WHERE username=?");
-            $stmt->bind_param("s", $username);
+    // Check if the login form was submitted
+    if (isset($_POST['login'])) {
+        // Get the user data from the form
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $remember = isset($_POST['remember']); // Check if the checkbox is checked
+
+        // Check if the username and password are correct
+        try {
+            $stmt = $db->prepare('SELECT * FROM users WHERE username=?');
+            $stmt->bind_param('s', $username);
             $stmt->execute();
             $result = $stmt->get_result();
             if ($result->num_rows == 1) {
                 $user = $result->fetch_assoc();
-                if (password_verify($password, $user["password"])) {
-                    echo "Anmeldung erfolgreich";
-                    // Hier können weitere Aktionen durchgeführt werden, z.B. eine Session starten
+                if (password_verify($password, $user['password'])) {
+                    echo 'Anmeldung erfolgreich';
+                    // Check if a session is already started
+                    if (session_status() != PHP_SESSION_ACTIVE) {
+                        // Start a session
+                        session_start();
+                    }
+                    // Store the username in the session variable
+                    $_SESSION['username'] = $username;
+                    // If the user wants to remember the device, set a cookie with the username and a random token
+                    if ($remember) {
+                        // Generate a random token
+                        $token = bin2hex(random_bytes(16));
+                        // Set the cookie for 30 days
+                        setcookie('username', $username, time() + 30 * 24 * 60 * 60);
+                        setcookie('token', $token, time() + 30 * 24 * 60 * 60);
+                        // Store the token in the database
+                        $stmt = $db->prepare('UPDATE users SET token=? WHERE username=?');
+                        $stmt->bind_param('ss', $token, $username);
+                        $stmt->execute();
+                    }
                 } else {
-                    echo "Falscher Benutzername oder Passwort";
+                    echo 'Falscher Benutzername oder Passwort';
                 }
             } else {
-                echo "Falscher Benutzername oder Passwort";
+                echo 'Falscher Benutzername oder Passwort';
             }
+        } catch (Exception $e) {
+            echo 'Ein Fehler ist aufgetreten: ' . $e->getMessage();
         }
+    }
     ?>
-    </p>
 
     <p>Noch nicht registriert? <a href="/registrierung.php">Registrieren</a></p>
 </body>
--- registrierung.php
+++ registrierung.php
@@ -17,55 +17,70 @@
         <input type="submit" name="register" value="Registrieren">
     </form>
 
-    <p>
     <?php
-        // Verbindung zur Datenbank herstellen
-        $db = new mysqli("hostname", "username", "password", "database_name");
+    // Hide errors
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
+    error_reporting(0);
 
-        // Überprüfen, ob die Verbindung erfolgreich war
-        if ($db->connect_error) {
-            die("Fehler beim Verbinden zur Datenbank: " . $db->connect_error);
-        }
+    // Connect to the database
+    try {
+        $db = new mysqli('hostname', 'username', 'password', 'database_name');
+    } catch (Exception $e) {
+        echo 'Fehler beim Verbinden zur Datenbank: ' . $e->getMessage();
+        exit;
+    }
 
-        // Überprüfen, ob das Formular abgeschickt wurde
-        if (isset($_POST["register"])) {
-            // Benutzerdaten aus dem Formular holen
-            $username = $_POST["username"];
-            $password = $_POST["password"];
-            $password2 = $_POST["password2"];
-            $email = $_POST["email"];
+    // Check if a session is already started
+    if (session_status() == PHP_SESSION_ACTIVE) {
+        // The user is already logged in, redirect to homepage
+        header('Location: /index.html');
+        exit;
+    }
 
-            // Überprüfen, ob alle Felder ausgefüllt wurden
-            if (empty($username) || empty($password) || empty($password2) || empty($email)) {
-                echo "Bitte alle Felder ausfüllen";
+    // Check if the register form was submitted
+    if (isset($_POST['register'])) {
+        // User data from the form
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $password2 = $_POST['password2'];
+        $email = $_POST['email'];
+
+        // Check if all fields are filled
+        if (empty($username) || empty($password) || empty($password2) || empty($email)) {
+            echo 'Bitte alle Felder ausfüllen';
+        } else {
+            // Check if the passwords match
+            if ($password != $password2) {
+                echo 'Die Passwörter stimmen nicht überein';
             } else {
-                // Überprüfen, ob die Passwörter übereinstimmen
-                if ($password != $password2) {
-                    echo "Die Passwörter stimmen nicht überein";
-                } else {
-                    // Überprüfen, ob der Benutzername bereits existiert
-                    $stmt = $db->prepare("SELECT * FROM users WHERE username=?");
-                    $stmt->bind_param("s", $username);
+                try {
+                    // Check if the username already exists
+                    $stmt = $db->prepare('SELECT * FROM users WHERE username=?');
+                    $stmt->bind_param('s', $username);
                     $stmt->execute();
                     $result = $stmt->get_result();
                     if ($result->num_rows > 0) {
-                        echo "Benutzername bereits vergeben";
+                        echo 'Benutzername bereits vergeben';
                     } else {
-                        // Benutzer in die Datenbank einfügen
+                        // Insert the user into the database
                         $password_hash = password_hash($password, PASSWORD_DEFAULT);
-                        $stmt = $db->prepare("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
-                        $stmt->bind_param("sss", $username, $password_hash, $email);
+                        $stmt = $db->prepare('INSERT INTO users (username, password, email) VALUES (?, ?, ?)');
+                        $stmt->bind_param('sss', $username, $password_hash, $email);
                         if ($stmt->execute()) {
-                            echo "Registrierung erfolgreich";
+                            echo 'Registrierung erfolgreich';
+                            header('Location: /anmeldung.php');
                         } else {
-                            echo "Fehler bei der Registrierung";
+                            echo 'Fehler bei der Registrierung';
                         }
                     }
+                } catch (Exception $e) {
+                    echo 'Ein Fehler ist aufgetreten: ' . $e->getMessage();
                 }
             }
         }
+    }
     ?>
-    </p>
 
     <p>Hast du bereits ein Konto? <a href="/anmeldung.php">Anmelden</a></p>
 </body>
